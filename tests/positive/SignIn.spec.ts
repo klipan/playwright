5eb3cd85dcbcc8c017f9432934bebbacd9ca7b28
klipan <nikola.klipa@yahoo.com>
2025-08-01 19:15:22 +0200

    small upd

--- tests/positive/SignIn.spec.ts
+++ tests/positive/SignIn.spec.ts
@@ -1,12 +1,10 @@
 import { test} from '@playwright/test';
 import { SigninPage } from '../../pages/signin.page';
-import { LoginPage } from '../../pages/login.page';
 import { generateRandomUser } from '../../randomthings/dataGenerator';
 import { saveUserToFile } from '../../randomthings/userStorage';
 
 test('@positive signin', async ({ page }) => {
   const signinPage = new SigninPage(page);
-  const loginPage = new LoginPage(page);
   const user = generateRandomUser();
   saveUserToFile(user);
   await signinPage.goto();
--- tests/positive/order.spec.ts
+++ tests/positive/order.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 import { LoginPage } from '../../pages/login.page';
 import {SigninPage} from "../../pages/signin.page";
 import {generateRandomUser} from "../../randomthings/dataGenerator";
